820fa98b39708433b08c57e68c58c5c9c08a15bd
Илья <149475177+Kusok0vody@users.noreply.github.com>
2024-02-03 12:46:01 +0700

    Приведение к каноническому (зачёркнуто) адекватному виду

--- gif_creation.py
+++ gif_creation.py
@@ -1,40 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from PIL import Image
 
-def anim_result(data_u:np.ndarray, timestep, L, H, picture:bool, showMe:bool):
+
+
+def anim_result(data_u:np.ndarray, timestep:float, L:float, H:float, picture:bool, showMe:bool):
     """
-    Make animation of u(t,x,y) and V(t,x,y) and save it to gif
+    Создание .gif файла
     """
 
-
-    def animate(i):                                                                                     # animate function for animation.FuncAnimation
-        line.set_array(data_u[i])                                                                       # change "slice" of u cube
-        ax.set_title(f'concentration field at t = {round(i*timestep,4)}')                                               # also change quiver 
+    def animate(i):
+        line.set_array(data_u[i])
+        ax.set_title(f'Поле концентрации при t = {round(i*timestep,4)}')
         return line
     
+
     fig, ax = plt.subplots()
-    line = plt.imshow(data_u[0], aspect = 'auto', cmap = 'turbo', extent = [0,L,0,H])             # u slice
+    line = plt.imshow(data_u[0], aspect = 'auto', cmap = 'turbo', extent = [0,L,0,H])
     plt.colorbar(line, ax=ax)                                                                   
-        # gradient for V
 
     ax.set_xlabel('x, m')
     ax.set_ylabel('y, m')
     ax.set_title('concentration field at t = {t}'.format(t=0))
 
-    ani = animation.FuncAnimation(fig, animate, interval=timestep*100, blit=False, frames = data_u.shape[0])       # animate
-
+    ani = animation.FuncAnimation(fig, animate, interval=timestep, blit=False, frames = data_u.shape[0])
 
     if showMe == True:
         plt.show()
 
     if picture == True:
-        writer = animation.PillowWriter(                                                                # saving picture
-            fps=60, metadata=dict(artist='Doofenshmirtz Evil Incorporated'), bitrate=1800)
+        writer = animation.PillowWriter(
+            fps=120, metadata=dict(artist='Приколисты'), bitrate=7200)
         ani.save(f"gifs/t={round(data_u.shape[0]*timestep,3)}, L={L}, H={H}.gif", writer=writer)
 
 
-def generate_init(path, shapeX, shapeY, norm):
+def generate_init(path:str, shapeX:int, shapeY:int, norm:float):
+    """
+    Генерация НУ из картинки
+    """
     image = Image.open(path).convert('L')
     new_image = image.resize((shapeX, shapeY))
     img = np.asarray(new_image)/norm
